398171406290adc8b806e4f34498cc4250b8db07
Eagle-008 <38664630+Eagle-008@users.noreply.github.com>
2025-11-07 14:01:25 +0300

    refactor: changed project structure

--- seminars/01_reddit_clone/redditclone/cmd/redditclone/main.go
+++ seminars/01_reddit_clone/redditclone/cmd/redditclone/main.go
@@ -8,22 +8,29 @@ import (
 )
 
 func main() {
-	staticHTMLHandler := http.FileServer(http.Dir("static/html"))
-	staticHandler := http.FileServer(http.Dir("static"))
+	staticHTMLHandler := http.FileServer(http.Dir("web/html"))
+	staticHandler := http.FileServer(http.Dir("web"))
 
-	mux := http.NewServeMux()
-	mux.Handle("/", staticHTMLHandler)
-	mux.Handle("/static/", http.StripPrefix("/static/", staticHandler))
+	staticMux := http.NewServeMux()
+	staticMux.Handle("/", staticHTMLHandler)
+	staticMux.Handle("/static/", http.StripPrefix("/static/", staticHandler))
+
+	apiMux := http.NewServeMux()
+
+	siteMux := http.NewServeMux()
+	siteMux.Handle("/", staticMux)
+	siteMux.Handle("/", apiMux)
 
 	log.Println("Starting server on :8080")
 	server := &http.Server{
 		Addr: ":8080",
 		//Handler:      stripTrailingSlash(mux),
-		Handler:      mux,
+		Handler:      siteMux,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
 	}
+
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
---
+++ seminars/01_reddit_clone/redditclone/internal/redditclone/api.go
@@ -0,0 +1,7 @@
+package main
+
+import "net/http"
+
+func registerApiHandler(w http.ResponseWriter, r *http.Request) {
+
+}
