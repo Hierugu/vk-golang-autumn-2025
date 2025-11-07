2535856120928f851e40dc3e12e17dbb8834f17b
Eagle-008 <38664630+Eagle-008@users.noreply.github.com>
2025-11-07 13:39:40 +0300

    feat: Seminar1 added fileServer to display static

--- seminars/01_reddit_clone/redditclone/cmd/redditclone/main.go
+++ seminars/01_reddit_clone/redditclone/cmd/redditclone/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	staticHTMLHandler := http.FileServer(http.Dir("static/html"))
+	staticHandler := http.FileServer(http.Dir("static"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", staticHTMLHandler)
+	mux.Handle("/static/", http.StripPrefix("/static/", staticHandler))
+
+	log.Println("Starting server on :8080")
+	server := &http.Server{
+		Addr: ":8080",
+		//Handler:      stripTrailingSlash(mux),
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
